9fdb0295abb7f32566acc073bcdf385455bf03a8
Christopher Boumenot <boumenot@gmail.com>
2015-05-15 22:11:56 -0700

    Add class loader for use with IKVM.

---
+++ src/Grobid/GrobidClassLoader.cs
@@ -0,0 +1,16 @@
+﻿using ikvm.runtime;
+using java.lang;
+
+namespace Grobid.NET
+{
+    namespace Grobid
+    {
+        public class GrobidClassLoader : ClassLoader
+        {
+            public GrobidClassLoader(ClassLoader parent)
+                : base(new AppDomainAssemblyClassLoader(typeof(GrobidClassLoader).Assembly))
+            {
+            }
+        }
+    }
+}
