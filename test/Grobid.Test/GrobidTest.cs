4ef4ae948fe7d6e042b093b8d3a53ab3312f3834
Christopher Boumenot <boumenot@gmail.com>
2015-05-15 23:44:01 -0700

    Simple unit test.

---
+++ test/Grobid.Test/GrobidTest.cs
@@ -0,0 +1,21 @@
+﻿using org.grobid.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Grobid.Test
+{
+    public class GrobidTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var x = GrobidModels.NAMES_HEADER;
+            x.toString().Should().Be("name/header");
+        }
+    }
+}
