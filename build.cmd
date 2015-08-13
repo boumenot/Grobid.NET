c2f66fc94fc89a5d2e2663c06c157d183983e0d2
Christopher Boumenot <boumenot@gmail.com>
2015-08-12 19:22:56 -0700

    Import paket; add NuGet references.

--- build.cmd
+++ build.cmd
@@ -1,17 +1,12 @@
 @echo off
-set config=%1
+cls
 
-if "%config%" == "" (
-   set config=Debug
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
 )
 
-msbuild.exe /p:Configuration="%config%" /p:Platform="x64" ^
-  /maxcpucount ^
-  /verbosity:minimal ^
-  /nologo ^
-  /fl ^
-  /flp:LogFile=build.log;Verbosity=detailed ^
-  build\build.proj
-  
-  
-  
+.paket\paket.exe restore 
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
