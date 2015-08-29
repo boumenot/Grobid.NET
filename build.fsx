01589d3cacedc7e02b41246d6c46054c15e03efe
Christopher Boumenot <boumenot@gmail.com>
2015-08-28 22:13:14 -0700

    NuGet target for Grobid models.

--- build.fsx
+++ build.fsx
@@ -1,14 +1,24 @@
 #r @"packages\FAKE\tools\FakeLib.dll"
+open System
+open System.IO
 open Fake
+open Fake.ReleaseNotesHelper
 open Fake.Testing
 
 let authors = ["Christopher Boumenot"]
+let project = "Grobid.Models"
+let summary = "You are a summary."
+let description = "You are a description."
 
 let projectName = "Grobid.NET"
 let projectDescription = "GROBID for .NET"
 let projectSummary = projectDescription
 
+let tags = ""
+
 let testResultsDir = "./testresults"
+
+let release = parseReleaseNotes (IO.File.ReadAllLines "RELEASE_NOTES.md")
 
 MSBuildDefaults <- {
   MSBuildDefaults with
@@ -46,6 +56,30 @@ Target "Zip" (fun _ ->
               |> Zip "content" "grobid.zip"
 )
 
+// ==================================================
+// NuGet
+
+// == grobid.X.Y.Z.nupkg
+
+// == grobid.models.X.Y.Z.nupkg
+
+Target "NuGet.Models" (fun _ ->
+                    NuGet (fun x ->
+                        { x with
+                            Authors = authors
+                            Project = project
+                            Summary = summary
+                            Description = description
+                            Version = release.NugetVersion
+                            ReleaseNotes = String.Join(Environment.NewLine, release.Notes)
+                            Tags = tags
+                            OutputPath = "publish"
+                            AccessKey = getBuildParamOrDefault "nugetkey" ""
+                            Publish = hasBuildParam "nugetkey"
+                            Files = [ (@"..\grobid.zip", Some "content", None) ] })
+                        ("nuget/" + project + ".nuspec")
+)
+
 Target "Default" DoNothing
 
 "Clean"
@@ -53,8 +87,5 @@ Target "Default" DoNothing
 
 "UnitTests"
   ==> "Default"
-
-"Zip"
-  ==> "BuildApp"
 
 RunTargetOrDefault "Default"
