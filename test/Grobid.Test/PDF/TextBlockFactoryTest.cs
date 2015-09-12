18cbf42ce910ddd193e3171ce0c8ffe1dbb19b3d
Christopher Boumenot <boumenot@gmail.com>
2015-09-12 00:53:17 -0700

    Block on same horizontal line are concatenated.

--- test/Grobid.Test/PDF/TextBlockFactoryTest.cs
+++ test/Grobid.Test/PDF/TextBlockFactoryTest.cs
@@ -1,13 +1,39 @@
 ﻿using FluentAssertions;
 using Grobid.NET;
+using iTextSharp.text.pdf.parser;
 using Xunit;
 
 namespace Grobid.Test
 {
     public class TextBlockFactoryTest
     {
+        public readonly LineSegment baseline = new LineSegment(new Vector(0, 0, 0), new Vector(0, 0, 0));
+        public readonly Vector bottomLeft = new Vector(0, 0, 0);
+        public readonly Vector topRight = new Vector(0, 0, 0);
+
         [Fact]
-        public void Test()
+        public void TestBlockShouldConcatenateBlocksOnSameLine()
+        {
+            var pageBlock = new PageBlock()
+            {
+                Height = 100,
+                TextInfos = new[]
+                {
+                    TextInfo.Create("The", this.baseline, this.bottomLeft, this.topRight),
+                    TextInfo.CreateEmpty(),
+                    TextInfo.Create("End", this.baseline, this.bottomLeft, this.topRight),
+                },
+            };
+
+            var testSubject = new TextBlockFactory();
+
+            var textBlocks = testSubject.Create(pageBlock);
+            textBlocks[0].Text.Should().Be("The End");
+        }
+
+
+        [Fact]
+        public void ParsePdfText()
         {
             var pageBlockFactory = new PageBackFactory();
             var pageBlocks = pageBlockFactory.Create(Sample.Pdf.OpenEssenseLinq());
