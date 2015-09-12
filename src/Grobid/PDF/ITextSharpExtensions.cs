bfdaa21302d4115cf582f702df9f093357370be5
Christopher Boumenot <boumenot@gmail.com>
2015-09-11 22:12:57 -0700

    Simple extensions to work with Vectors.

---
+++ src/Grobid/PDF/ITextSharpExtensions.cs
@@ -0,0 +1,10 @@
+﻿using iTextSharp.text.pdf.parser;
+
+namespace Grobid.NET
+{
+    public static class ITextSharpExtensions
+    {
+        public static float X(this Vector v) { return v[Vector.I1]; }
+        public static float Y(this Vector v) { return v[Vector.I2]; }
+    }
+}
