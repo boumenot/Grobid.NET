22b8ee671d8c3f3fed37b11fc5a277b403b281d1
Christopher Boumenot <boumenot@gmail.com>
2015-09-12 00:33:27 -0700

    Simple integration test for TextBlockFactory.

---
+++ test/Grobid.Test/PDF/TextBlockFactoryTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Grobid.NET;
+using Xunit;
+
+namespace Grobid.Test
+{
+    public class TextBlockFactoryTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var pageBlockFactory = new PageInfoFactory();
+            var pageBlocks = pageBlockFactory.Create(Sample.Pdf.OpenEssenseLinq());
+
+            var testSubject = new TextBlockFactory();
+            var textBlocks = testSubject.Create(pageBlocks[0].TextInfos, pageBlocks[0].Height);
+            textBlocks.Should().HaveCount(118);
+        }
+    }
+}
