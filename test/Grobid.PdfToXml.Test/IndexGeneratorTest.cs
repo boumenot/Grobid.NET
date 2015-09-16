6cbdcb5a394e95effcfe17208f9cc4ec86ee10e3
Christopher Boumenot <boumenot@gmail.com>
2015-09-15 23:03:25 -0700

    Start implementing a class to track indexes.

---
+++ test/Grobid.PdfToXml.Test/IndexGeneratorTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Grobid.PdfToXml.Test
+{
+    public class IndexGeneratorTest
+    {
+        [Fact]
+        public void PageIndexShouldIncrementOnCall()
+        {
+            var testSubject = new IndexGenerator();
+            testSubject.PageIndex.Should().Be("p1");
+            testSubject.PageIndex.Should().Be("p2");
+            testSubject.PageIndex.Should().Be("p3");
+        }
+
+        [Fact]
+        public void TextIndexShouldIncrementOnCall()
+        {
+            var testSubject = new IndexGenerator();
+            testSubject.PageIndex.Should().Be("p1");
+            testSubject.TextIndex.Should().Be("p1_t1");
+            testSubject.TextIndex.Should().Be("p1_t2");
+            testSubject.TextIndex.Should().Be("p1_t3");
+        }
+    }
+
+    public class IndexGenerator
+    {
+        private int pageIndex;
+        private int textIndex;
+
+        public string PageIndex
+        {
+            get
+            {
+                this.pageIndex++;
+                return String.Format("p{0}", this.pageIndex);
+            }
+        }
+
+        public string TextIndex
+        {
+            get
+            {
+                this.textIndex++;
+                return String.Format("p{0}_t{1}", this.pageIndex, this.textIndex);
+            }
+        }
+    }
+}
