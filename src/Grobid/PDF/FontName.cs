250f568f48e627e78a0a86fb62e1a40cd29e36f8
Christopher Boumenot <boumenot@gmail.com>
2015-09-15 22:24:48 -0700

    Add class to represent a font name.

---
+++ src/Grobid/PDF/FontName.cs
@@ -0,0 +1,25 @@
+﻿namespace Grobid.NET
+{
+    public class FontName
+    {
+        private FontName() {}
+
+        public string Tag { get; private set; }
+        public string Name { get; private set; }
+        public string Weight { get; private set; }
+
+        public static FontName Parse(string name)
+        {
+            var fontName = new FontName();
+
+            int indexOfPlusSign = name.IndexOf('+');
+            int indexOfMinusSign = name.IndexOf('-');
+
+            fontName.Tag = name.Substring(0, indexOfPlusSign);
+            fontName.Name = name.Substring(indexOfPlusSign + 1, indexOfMinusSign - indexOfPlusSign - 1);
+            fontName.Weight = name.Substring(indexOfMinusSign + 1, name.Length - indexOfMinusSign - 1);
+
+            return fontName;
+        }
+    }
+}
---
+++ test/Grobid.Test/PDF/FontNameTest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Grobid.NET;
+using Xunit;
+
+namespace Grobid.Test.PDF
+{
+    public class FontNameTest
+    {
+        [Fact]
+        public void ParseShouldReturnFontName()
+        {
+            var testSubject = FontName.Parse("CHUFSU+NimbusRomNo9L-Medi");
+            testSubject.Tag.Should().Be("CHUFSU");
+            testSubject.Name.Should().Be("NimbusRomNo9L");
+            testSubject.Weight.Should().Be("Medi");
+        }
+    }
+}
