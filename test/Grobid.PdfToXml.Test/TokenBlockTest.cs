140d6091c47a4554d3361a032e15f1c94c21733d
Christopher Boumenot <boumenot@gmail.com>
2015-09-17 22:27:07 -0700

    Remove dead tests.

--- test/Grobid.PdfToXml.Test/TokenBlockTest.cs
+++ test/Grobid.PdfToXml.Test/TokenBlockTest.cs
@@ -64,31 +64,5 @@ namespace Grobid.PdfToXml.Test
             testSubject.BoundingRectangle.Width.Should().Be(6);
             testSubject.Width.Should().Be(6);
         }
-
-        //[Fact]
-        //public void CreateTextBlockIsEmptyShouldBeFalse()
-        //{
-        //    var testSubject = TokenBlock.Create(null, null, new Vector(0, 0, 0), new Vector(0, 0, 0));
-        //    testSubject.IsEmpty.Should().BeFalse();
-        //}
-
-        //[Fact]
-        //public void CreateTextBlockText()
-        //{
-        //    var testSubject = TokenBlock.Create("text", null, new Vector(0, 0, 0), new Vector(0, 0, 0));
-        //    testSubject.Text.Should().Be("text");
-        //}
-
-        //[Fact]
-        //public void CreateTextBlockLineSegment()
-        //{
-        //    var lineSegment = new LineSegment(
-        //        new Vector(0, 1, 0),
-        //        new Vector(2, 3, 0));
-
-        //    var testSubject = TokenBlock.Create(null, lineSegment, new Vector(0, 0, 0), new Vector(0, 0, 0));
-        //    testSubject.StartPoint.Should().Be(lineSegment.GetStartPoint());
-        //    testSubject.EndPoint.Should().Be(lineSegment.GetEndPoint());
-        //}
     }
 }
