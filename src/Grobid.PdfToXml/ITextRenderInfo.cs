9701d45cd4e0b4a2a6ac30bca8530699a10e5737
Christopher Boumenot <boumenot@gmail.com>
2015-10-05 11:13:27 -0700

    Introduce interface for TextRenderInfo, and wrapper class.

---
+++ src/Grobid.PdfToXml/ITextRenderInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace Grobid.PdfToXml
+{
+    public interface ITextRenderInfo
+    {
+        PdfString PdfString { get; }
+
+        LineSegment GetAscentLine();
+
+        LineSegment GetBaseline();
+
+        IList<TextRenderInfo> GetCharacterRenderInfos();
+
+        LineSegment GetDescentLine();
+
+        BaseColor GetFillColor();
+
+        DocumentFont GetFont();
+
+        int? GetMcid();
+
+        float GetRise();
+
+        float GetSingleSpaceWidth();
+
+        BaseColor GetStrokeColor();
+
+        string GetText();
+
+        int GetTextRenderMode();
+
+        LineSegment GetUnscaledBaseline();
+
+        bool HasMcid(int mcid);
+
+        bool HasMcid(int mcid, bool checkTheTopmostLevelOnly);
+    }
+}
---
+++ src/Grobid.PdfToXml/TextRenderInfoWrapper.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace Grobid.PdfToXml
+{
+    internal class TextRenderInfoWrapper : ITextRenderInfo
+    {
+        private readonly TextRenderInfo textRenderInfo;
+
+        public TextRenderInfoWrapper(TextRenderInfo textRenderInfo)
+        {
+            this.textRenderInfo = textRenderInfo;
+        }
+
+        public PdfString PdfString
+        {
+            get { return this.textRenderInfo.PdfString; }
+        }
+
+        public LineSegment GetAscentLine()
+        {
+            return this.textRenderInfo.GetAscentLine();
+        }
+
+        public LineSegment GetBaseline()
+        {
+            return this.textRenderInfo.GetBaseline();
+        }
+
+        public IList<TextRenderInfo> GetCharacterRenderInfos()
+        {
+            return this.textRenderInfo.GetCharacterRenderInfos();
+        }
+
+        public LineSegment GetDescentLine()
+        {
+            return this.textRenderInfo.GetDescentLine();
+        }
+
+        public BaseColor GetFillColor()
+        {
+            return this.textRenderInfo.GetFillColor();
+        }
+
+        public DocumentFont GetFont()
+        {
+            return this.textRenderInfo.GetFont();
+        }
+
+        public int? GetMcid()
+        {
+            return this.textRenderInfo.GetMcid();
+        }
+
+        public float GetRise()
+        {
+            return this.textRenderInfo.GetRise();
+        }
+
+        public float GetSingleSpaceWidth()
+        {
+            return this.textRenderInfo.GetSingleSpaceWidth();
+        }
+
+        public BaseColor GetStrokeColor()
+        {
+            return this.textRenderInfo.GetStrokeColor();
+        }
+
+        public string GetText()
+        {
+            return this.textRenderInfo.GetText();
+        }
+
+        public int GetTextRenderMode()
+        {
+            return this.textRenderInfo.GetTextRenderMode();
+        }
+
+        public LineSegment GetUnscaledBaseline()
+        {
+            return this.textRenderInfo.GetUnscaledBaseline();
+        }
+
+        public bool HasMcid(int mcid)
+        {
+            return this.textRenderInfo.HasMcid(mcid);
+        }
+
+        public bool HasMcid(int mcid, bool checkTheTopmostLevelOnly)
+        {
+            return this.textRenderInfo.HasMcid(mcid, checkTheTopmostLevelOnly);
+        }
+    }
+}
