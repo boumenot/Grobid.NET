45571fd2475781a2d04bc4cd90cc43be4717a887
Christopher Boumenot <boumenot@gmail.com>
2015-10-05 11:57:54 -0700

    Write test to ensure characters are normalized.

--- test/Grobid.PdfToXml.Test/TokenBlockFactoryTest.cs
+++ test/Grobid.PdfToXml.Test/TokenBlockFactoryTest.cs
@@ -2,6 +2,8 @@
 
 using FluentAssertions;
 
+using iTextSharp.text.pdf.parser;
+
 using Xunit;
 
 namespace Grobid.PdfToXml.Test
@@ -9,13 +11,22 @@ namespace Grobid.PdfToXml.Test
     public class TokenBlockFactoryTest
     {
         [Fact]
-        public void TestA()
+        public void FactoryShouldNormalizeUnicodeStrings()
         {
-            var stub = new TextRenderInfoStub();
-            var testSubject = new TokenBlockFactory(100, 100);
+            var stub = new TextRenderInfoStub
+            {
+                AscentLine = new LineSegment(new Vector(0, 0, 0), new Vector(1, 1, 1)),
+                Baseline = new LineSegment(new Vector(0, 0, 0), new Vector(1, 1, 1)),
+                DescentLine = new LineSegment(new Vector(0, 0, 0), new Vector(1, 1, 1)),
+                PostscriptFontName = "CHUFSU+NimbusRomNo9L-Medi",
+                Text = "abcd\u0065\u0301fgh",
+            };
 
-            Action test = () => testSubject.Create(stub);
-            test.ShouldThrow<NullReferenceException>("I am still implementing the code.");
+            var testSubject = new TokenBlockFactory(100, 100);
+            var tokenBlock = testSubject.Create(stub);
+
+            stub.Text.ToCharArray().Should().HaveCount(9);
+            tokenBlock.Text.ToCharArray().Should().HaveCount(8);
         }
     }
 }
