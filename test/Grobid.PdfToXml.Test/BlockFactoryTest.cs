075830dc0eb8418fac21aa81d7bab2a967a42a90
Christopher Boumenot <boumenot@gmail.com>
2016-09-07 21:34:18 -0700

    Group text blocks into blocks
    Just a prototype so far.

---
+++ test/Grobid.PdfToXml.Test/BlockFactoryTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Grobid.PdfToXml.Test
+{
+
+    public class BlockFactoryTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var testInfoFactory = new PageBlockFactory();
+            var pageBlocks = testInfoFactory.Create(Sample.Pdf.OpenEssenseLinq(), 1);
+            var textBlocks = pageBlocks.First().TextBlocks;
+
+            var index = 3;
+
+            var textBlockA = textBlocks[index - 1];
+            var textBlockB = textBlocks[index + 0];
+            var textBlockC = textBlocks[index + 1];
+
+            textBlockA.IsSameBlock(textBlockB).Should().BeTrue();
+            textBlockB.IsSameBlock(textBlockC).Should().BeFalse();
+        }
+    }
+
+    public static class Extensions
+    {
+        // Same rules as pdftoxml.
+
+        // Max difference in primary font sizes on two lines in the same
+        // block.  Delta1 is used when examining new lines above and below the
+        // current block.
+        private const double maxBlockFontSizeDelta1 = 0.05;
+
+        // Max distance between baselines of two lines within a block, as a
+        // fraction of the font size.
+        private const double maxLineSpacingDelta = 1.5;
+
+        public static bool IsSameBlock(this TextBlock textBlockA, TextBlock textBlockB)
+        {
+            bool isA = textBlockB.Y + textBlockB.Height >= textBlockA.Y;
+            bool isB = Math.Abs(textBlockB.TokenBlocks[0].FontSize - textBlockA.TokenBlocks[0].FontSize) <
+                       (textBlockB.TokenBlocks[0].FontSize * Extensions.maxBlockFontSizeDelta1);
+            bool isC = (textBlockB.Y - textBlockA.Y) < (textBlockA.TokenBlocks[0].FontSize * maxLineSpacingDelta);
+
+            return (isA && isB && isC);
+        }
+    }
+}
--- test/Grobid.Test/FeatureTesterTest.cs
+++ test/Grobid.Test/FeatureTesterTest.cs
@@ -1,11 +1,7 @@
-﻿using Grobid.NET;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
+
+using Grobid.NET;
 
 namespace Grobid.Test
 {
