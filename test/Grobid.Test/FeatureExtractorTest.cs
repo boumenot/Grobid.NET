3861a244222ff21036ff7e591a3010f15978ad42
Christopher Boumenot <boumenot@gmail.com>
2016-09-09 23:43:41 -0700

    Implement FeatureExtractor
    Implement Prefix and Suffix

---
+++ test/Grobid.Test/FeatureExtractorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+using Xunit;
+
+namespace Grobid.Test
+{
+    public class FeatureExtractorTest
+    {
+        [Fact]
+        public void TestPrefix()
+        {
+            var testSubject = new FeatureExtractor();
+            testSubject.Prefix("Christopher", 1).Should().Be("C");
+            testSubject.Prefix("Christopher", 2).Should().Be("Ch");
+            testSubject.Prefix("Christopher", 3).Should().Be("Chr");
+
+            testSubject.Prefix("abc", 4).Should().Be("abc");
+            testSubject.Prefix("abc", 3).Should().Be("abc");
+        }
+
+        [Fact]
+        public void TestSuffix()
+        {
+            var testSubject = new FeatureExtractor();
+            testSubject.Suffix("Christopher", 1).Should().Be("r");
+            testSubject.Suffix("Christopher", 2).Should().Be("er");
+            testSubject.Suffix("Christopher", 3).Should().Be("her");
+
+            testSubject.Suffix("abc", 4).Should().Be("abc");
+            testSubject.Suffix("abc", 3).Should().Be("abc");
+        }
+    }
+
+    public class FeatureExtractor {
+        public string Prefix(string s, int length)
+        {
+            return s.Substring(0, Math.Min(s.Length, length));
+        }
+
+        public string Suffix(string s, int length)
+        {
+            int len = Math.Min(s.Length, length);
+            int offset = s.Length - len;
+
+            return s.Substring(offset, len);
+        }
+    }
+}
