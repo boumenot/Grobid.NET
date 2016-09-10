318c9a3b0f4a9f6f6d0812db4e2230bdb370b52c
Christopher Boumenot <boumenot@gmail.com>
2016-09-09 20:27:00 -0700

    reorder namepsaces

--- test/Grobid.Test/LexiconFactoryTest.cs
+++ test/Grobid.Test/LexiconFactoryTest.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
+
 using Grobid.NET;
 
 namespace Grobid.Test
