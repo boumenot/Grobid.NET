bd3f2b20dd0c5caa9917f602db341153150b464c
Christopher Boumenot <boumenot@gmail.com>
2016-10-02 11:23:45 -0700

    Use types not strings
    Use a type to represent the result of TeiFeatureFactory instead of string.

---
+++ test/Grobid.Test/TeiFeatureFormatterTest.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Xunit;
+
+using Grobid.NET;
+
+
+namespace Grobid.Test
+{
+    public class TeiFeatureFormatterTest
+    {
+        [Fact]
+        public void TeiFeatureFormatter00()
+        {
+            var feature = new TeiFeature
+            {
+                Value = "The",
+                Classification = "title",
+                TokenIndex = 0,
+            };
+
+            var testSubject = new TeiFeatureFormatter();
+            testSubject.Format(feature).Should().Be("The I-<title>");
+            testSubject.Format(new [] { feature }).Should().HaveCount(1).And.Contain("The I-<title>");
+        }
+
+        [Fact]
+        public void TeiFeatureFormatter01()
+        {
+            var feature = new TeiFeature
+            {
+                Value = "The",
+                Classification = "title",
+                TokenIndex = 1,
+            };
+
+            var testSubject = new TeiFeatureFormatter();
+            testSubject.Format(feature).Should().Be("The <title>");
+            testSubject.Format(new[] { feature }).Should().HaveCount(1).And.Contain("The <title>");
+        }
+
+        [Fact]
+        public void TeiFeatureFormatter02()
+        {
+            var feature = new TeiFeature
+            {
+                Value = "@newline",
+            };
+
+            var testSubject = new TeiFeatureFormatter();
+            testSubject.Format(feature).Should().Be("@newline");
+            testSubject.Format(new[] { feature }).Should().HaveCount(1).And.Contain("@newline");
+        }
+    }
+}
