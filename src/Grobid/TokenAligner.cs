9c9e9a7cd665ccfa6416e3c10d9fe783d899c237
Christopher Boumenot <boumenot@gmail.com>
2016-10-01 23:53:06 -0700

    Implement a TokenAligner
    The TokenAligner class is used to align two token streams of differing
    types based on a common token type.

---
+++ src/Grobid/TokenAligner.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grobid.NET
+{
+    /// <summary>
+    /// Align two streams of tokens.
+    /// </summary>
+    /// <typeparam name="TItem">Type of token to align two streams based on.</typeparam>
+    /// <remarks>
+    /// <para>
+    /// Given a collection of type T, and another collection of type U we want
+    /// to align the two streams based on a common type between the two, type 
+    /// V.  We attempt to align based on a user specified window size.  If a
+    /// matching token cannot be found within the window, we advance the 
+    /// collection of type T by one and repeat.  Alignment continues until the
+    /// token stream is exhausted.
+    /// </para>
+    /// <para>
+    /// When the token streams are aligned, a transform of the T, U, and V is
+    /// computed and returned.
+    /// </para>
+    /// <para>
+    /// All of this is really fancy and confusing way to say we want to match
+    /// two sets of strings, and return a new string.  There is too much variation
+    /// in what the strings are hence the need for all of the genericity.
+    /// </para>
+    /// </remarks>
+    public class TokenAligner<TItem>
+        where TItem : IEquatable<TItem>
+    {
+        public const int DefaultWindowSize = 7;
+        private readonly int windowSize;
+
+        public TokenAligner(int windowSize = TokenAligner<TItem>.DefaultWindowSize)
+        {
+            this.windowSize = windowSize;
+        }
+
+        public IEnumerable<TResult> Align<TSource1, TSource2, TResult>(
+            IReadOnlyList<TSource1> xs, Func<TSource1, TItem> getX,
+            IReadOnlyList<TSource2> ys, Func<TSource2, TItem> getY,
+            Func<TSource1, TSource2, TItem, TResult> transform)
+        {
+            // Index of the last matched token for the source stream.
+            int lastIndex = 0;
+            for (int i = 0; i < xs.Count; i++)
+            {
+                var tokenX = getX(xs[i]);
+
+                for (int windowOffset = lastIndex;
+                     windowOffset < ys.Count && windowOffset < lastIndex + this.windowSize;
+                     windowOffset++)
+                {
+                    var tokenY = getY(ys[windowOffset]);
+                    if (tokenX.Equals(tokenY))
+                    {
+                        yield return transform(xs[i], ys[windowOffset], tokenX);
+                        lastIndex = windowOffset + 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/Grobid.Test/TokenAlignerTest.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+
+using FluentAssertions;
+using Xunit;
+
+using Grobid.NET;
+
+namespace Grobid.Test
+{
+    public class TokenAlignerTest
+    {
+        [Fact]
+        public void TokenAligner00()
+        {
+            var source = new[] { "abc", "def" };
+            var target = new[] { "abc", "def" };
+
+            this.Align(source, target).Should().BeEquivalentTo("abc", "def");
+        }
+
+        [Fact]
+        public void TokenAligner01()
+        {
+            var source = new[] { "abc", "ghi" };
+            var target = new[] { "abc", "def" };
+
+            this.Align(source, target).Should().BeEquivalentTo("abc");
+        }
+
+        [Fact]
+        public void TokenAligner02()
+        {
+            var source = new[] { "def", "ghi" };
+            var target = new[] { "abc", "def" };
+
+            this.Align(source, target).Should().BeEquivalentTo("def");
+        }
+
+        [Fact]
+        public void TokenAligner03()
+        {
+            var source = new[] { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            var target = new[] { "cat", "dog", "bird", "deer", "bison", "lion", "gazelle" };
+
+            this.Align(source, target).Should().BeEmpty();
+        }
+
+        [Fact]
+        public void TokenAligner04()
+        {
+            var source = new[] { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            var target = new[] { "abc", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+            this.Align(source, target).Should().BeEquivalentTo(target);
+        }
+
+        [Fact]
+        public void TokenAligner05()
+        {
+            var source = new[] { "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            var target = new[] { "abc", "wxyz" };
+
+            this.Align(source, target).Should().BeEquivalentTo("abc", "wxyz");
+            this.Align(target, source).Should().BeEquivalentTo(target);
+        }
+
+        [Fact]
+        public void TokenAligner06()
+        {
+            var source = new[] { "3", "4", "5", "7", "15" };
+            var target = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" };
+
+            this.Align(source, target).Should().BeEquivalentTo("3", "4", "5", "7");
+        }
+
+        [Fact]
+        public void TokenAligner07()
+        {
+            var source1 = new[] { "1", "2" };
+            var target1 = new[] { "1", ".", ".", "2" };
+
+            this.Align(source1, target1, 3).Should().BeEquivalentTo("1", "2");
+            this.Align(source1, target1, 2).Should().BeEquivalentTo("1");
+            this.Align(source1, target1, 1).Should().BeEquivalentTo("1");
+            this.Align(source1, target1, 0).Should().BeEquivalentTo();
+        }
+
+        private string[] Align(string[] source, string[] target, int windowSize = TokenAligner<string>.DefaultWindowSize)
+        {
+            var testSubject = new TokenAligner<string>(windowSize);
+            var results = testSubject.Align(
+                source,
+                x => x,
+                target,
+                x => x,
+                (src, tgt, x) => x);
+
+            return results.ToArray();
+        }
+    }
+}
