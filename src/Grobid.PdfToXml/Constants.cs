69bfbe0a534f443e7c30eca9f96fb025ae4f8adc
Christopher Boumenot <boumenot@gmail.com>
2016-10-01 20:38:34 -0700

    Full punctuation should include the opening paren

--- src/Grobid.PdfToXml/Constants.cs
+++ src/Grobid.PdfToXml/Constants.cs
@@ -2,6 +2,6 @@
 {
     public static class Constants
     {
-        public static char[] FullPunctuation = "[ ,:;?.!/)-–\"“”‘’'`$]*♦♥♣♠".ToCharArray();
+        public static char[] FullPunctuation = "[ ,:;?.!/()-–\"“”‘’'`$]*♦♥♣♠".ToCharArray();
     }
 }
