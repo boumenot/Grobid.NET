cc8002d0094c1ccf089ac133d68c2bc883fd998a
Christopher Boumenot <boumenot@gmail.com>
2016-10-03 21:40:17 -0700

    Implement TrainerRow
    TrainerRow is a feature row returned by a CRP engine that contains the
    expected value and the obtained value.  The expected value is extracted
    from a known good source (TEI header).  The obtained value is calculated
    based on the features of the row and a trained model.
    
    A trainer row is similar to the following
    
     The the 0 1 0 1 0 BLOCKIN LINESTART 0 1 <title> <title>
    
    A trainer row is almost identical to a feature row (and duplicates most of
    the code).  A feature row has one classification whereas a trainer has
    two.
    
    Consider ways to merge these classes.

---
+++ src/Grobid/TrainerRow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Grobid.NET
+{
+    public class TrainerRow {
+        public string Expected { get; set; }
+        public string Obtained { get; set; }
+        public string Value { get; set; }
+
+        public static TrainerRow Parse(string s)
+        {
+            var expected = TrainerRow.ExtractClassification(s, 0);
+            var obtained = TrainerRow.ExtractClassification(s, expected.Item2 + 1);
+
+            var row = new TrainerRow
+            {
+                Expected = expected.Item1,
+                Obtained = obtained.Item1,
+                Value = TrainerRow.ExtractValue(s),
+            };
+
+            return row;
+        }
+
+        private static Tuple<string, int> ExtractClassification(string s, int startingIndex)
+        {
+            int begin = s.IndexOf("<", startingIndex);
+            if (begin == -1)
+            {
+                throw new ArgumentException($"Cannot locate end of classification token in '{s}'");
+            }
+
+            int end = s.IndexOf(">", begin);
+            if (end == -1)
+            {
+                throw new ArgumentException($"Cannot locate end of classification token in '{s}'");
+            }
+
+            // +1 == eat extraneous '<'
+            // -1 == eat extraneous '>'
+            var classification = s.Substring(begin + 1, end - begin - 1);
+            return Tuple.Create(classification, begin);
+        }
+
+        private static string ExtractValue(string s)
+        {
+            int end = s.IndexOf(" ");
+            if (end == -1 || string.IsNullOrWhiteSpace(s) || s[0] == '<')
+            {
+                throw new ArgumentException($"Cannot locate value in'{s}'");
+            }
+
+            return s.Substring(0, end);
+        }
+    }
+}
---
+++ test/Grobid.Test/TrainerRowTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using FluentAssertions;
+using Xunit;
+
+using Grobid.NET;
+
+namespace Grobid.Test
+{
+    public class TrainerRowTest
+    {
+        [Fact]
+        public void TrainerRowAcceptableValues()
+        {
+            var rows = new[]
+            {
+                "The the 1 1 0 1 0 LINESTART 0 1 <title> <title>",
+                "The the 1 1 0 1 0 LINESTART 0 1 <title> I-<title>",
+                "The the 1 1 0 1 0 LINESTART 0 1 I-<title> <title>",
+                "The the 1 1 0 1 0 LINESTART 0 1 I-<title> I-<title>",
+            };
+
+            foreach (var row in rows)
+            {
+                var testSubject = TrainerRow.Parse(row);
+
+                testSubject.Expected.Should().Be("title");
+                testSubject.Obtained.Should().Be("title");
+                testSubject.Value.Should().Be("The");
+            }
+        }
+
+        [Fact]
+        public void TrainerRowDifferentClassifications()
+        {
+            var testSubject = TrainerRow.Parse("The the 1 1 0 1 0 LINESTART 0 1 <expected> <obtained>");
+
+            testSubject.Expected.Should().Be("expected");
+            testSubject.Obtained.Should().Be("obtained");
+            testSubject.Value.Should().Be("The");
+        }
+
+        [Fact]
+        public void TrainerRowMalformedValues()
+        {
+            var rows = new[]
+            {
+                "The the 1 1 0 1 0 LINESTART 0 1 title",          // no expected value
+                "The the 1 1 0 1 0 LINESTART 0 1 title>",         // no '<'
+                "The the 1 1 0 1 0 LINESTART 0 1 <title>",        // no obtained value
+                "The the 1 1 0 1 0 LINESTART 0 1 <title> <title", // incomplete obtained value
+                "The the 1 1 0 1 0 LINESTART 0 1 >title<",        // '>' before '<'
+                "The\tthe\t1\t1\t0\t1\t0\tLINESTART\t0\t1\t<title>\t<title>", // tab separator instead of spaces
+                "<title>",                                        // no value
+                "<title> <title>",                                // no value
+            };
+
+            foreach (var row in rows)
+            {
+                Action action = () => TrainerRow.Parse(row);
+                action.ShouldThrow<ArgumentException>(row);
+            }
+        }
+    }
+}
