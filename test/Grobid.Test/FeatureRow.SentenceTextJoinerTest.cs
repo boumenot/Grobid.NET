c0822215ea31283003bc57247376654435943a7f
Christopher Boumenot <boumenot@gmail.com>
2016-11-25 21:09:36 -0800

    Start of a sentence joiner

---
+++ test/Grobid.Test/FeatureRow.SentenceTextJoinerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Grobid.NET;
+
+using Xunit;
+
+namespace Grobid.Test
+{
+    public class SentenceTextJoinerTest
+    {
+        [Fact]
+        public void SimpleText()
+        {
+            var testSubject = new SentenceTextJoiner();
+            var featureRows = new[]
+            {
+                new FeatureRow() { Classification = "title", Value = "The" },
+                new FeatureRow() { Classification = "title", Value = "Big" },
+                new FeatureRow() { Classification = "title", Value = "Bad" },
+                new FeatureRow() { Classification = "title", Value = "Title" },
+            };
+
+            var title = testSubject.Join(featureRows);
+            title.Should().Be("The Big Bad Title");
+        }
+    }
+
+    public class SentenceTextJoiner
+    {
+        public string Join(FeatureRow[] featureRows)
+        {
+            var s = String.Join(" ", featureRows.Select(x => x.Value));
+            return s;
+        }
+    }
+}
