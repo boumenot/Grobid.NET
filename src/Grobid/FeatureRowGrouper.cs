e8544ff261b29fb4a7c89b4d8870b159cc1ecba6
Christopher Boumenot <boumenot@gmail.com>
2016-11-25 22:54:34 -0800

    Move FeatureRowGrouper to Grobid project

---
+++ src/Grobid/FeatureRowGrouper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grobid.NET
+{
+    public class FeatureRowGrouper
+    {
+        public Dictionary<string, List<ArraySegment<FeatureRow>>> Group(FeatureRow[] featureRows)
+        {
+            var groups = this.Break(featureRows)
+                .GroupBy(x => x.Item1)
+                .ToDictionary(
+                    x => x.Key,
+                    x => x.Select(y => y.Item2).ToList());
+
+            return groups;
+        }
+
+        private IEnumerable<Tuple<string, ArraySegment<FeatureRow>>> Break(FeatureRow[] featureRows)
+        {
+            int offset = 0;
+            while (offset < featureRows.Length)
+            {
+                var featureRow = featureRows.Skip(offset).First();
+                var startingOffset = featureRow.IsStart ? 1 : 0;
+
+                var length = featureRows
+                    .Skip(offset + startingOffset)
+                    .TakeWhile(x => !x.IsStart && x.Classification == featureRow.Classification)
+                    .Count();
+
+                yield return Tuple.Create(
+                    featureRow.Classification,
+                    new ArraySegment<FeatureRow>(featureRows, offset, length + startingOffset));
+
+                offset += length + startingOffset;
+            }
+        }
+    }
+}
--- test/Grobid.Test/FeatureRowGrouperTest.cs
+++ test/Grobid.Test/FeatureRowGrouperTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using ApprovalTests;
+﻿using ApprovalTests;
 using ApprovalTests.Reporters;
 using Newtonsoft.Json;
 using Xunit;
@@ -78,41 +74,6 @@ namespace Grobid.Test
 
             var groups = testSubject.Group(featureRows);
             Approvals.VerifyJson(JsonConvert.SerializeObject(groups));
-        }
-    }
-
-    public class FeatureRowGrouper
-    {
-        public Dictionary<string, List<ArraySegment<FeatureRow>>> Group(FeatureRow[] featureRows)
-        {
-            var groups = this.Break(featureRows)
-                .GroupBy(x => x.Item1)
-                .ToDictionary(
-                    x => x.Key,
-                    x => x.Select(y => y.Item2).ToList());
-
-            return groups;
-        }
-
-        private IEnumerable<Tuple<string, ArraySegment<FeatureRow>>> Break(FeatureRow[] featureRows)
-        {
-            int offset = 0;
-            while (offset < featureRows.Length)
-            {
-                var featureRow = featureRows.Skip(offset).First();
-                var startingOffset = featureRow.IsStart ? 1 : 0;
-
-                var length = featureRows
-                    .Skip(offset + startingOffset)
-                    .TakeWhile(x => !x.IsStart && x.Classification == featureRow.Classification)
-                    .Count();
-
-                yield return Tuple.Create(
-                    featureRow.Classification,
-                    new ArraySegment<FeatureRow>(featureRows, offset, length + startingOffset));
-
-                offset += length + startingOffset;
-            }
         }
     }
 }
