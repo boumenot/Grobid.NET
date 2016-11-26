47142df326a0b1fd7cabf26afc6577132f0d2e57
Christopher Boumenot <boumenot@gmail.com>
2016-11-25 21:01:31 -0800

    Rename email factory to text joiner

---
+++ src/Grobid/FeatureRowTextJoiner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Grobid.NET
+{
+    public class FeatureRowTextJoiner
+    {
+        public string Join(FeatureRow[] featureRows)
+        {
+            return String.Join(string.Empty, featureRows.Select(x => x.Value));
+        }
+    }
+}
