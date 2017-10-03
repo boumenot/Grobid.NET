127ba352a39fabe725b2f8d68a1ceb7cf6c52be1
Christopher Boumenot <boumenot@gmail.com>
2017-10-03 00:32:36 -0700

    DateFeatureFactory for date model training

---
+++ src/Grobid/DateFeatureFactory.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+using Grobid.PdfToXml;
+
+namespace Grobid.NET
+{
+    // Extract dates from a XML document of the following form.
+    //
+    // <dates>
+    //   <date><month>March</month> <day>28</day>, <year>2007</year></date>
+    // </dates>
+    //
+    // An XML document may have more than one <date /> element.
+
+    public sealed class DateFeatureFactory
+    {
+        public IEnumerable<TeiFeature[]> Create(XDocument doc)
+        {
+            foreach (var nodes in doc.XPathSelectElements("/dates/date"))
+            {
+                var features = new List<TeiFeature>();
+
+                foreach (var node in nodes.DescendantNodes())
+                {
+                    var ele = node as XElement;
+                    if (ele != null && ele.Name.LocalName != "lb")
+                    {
+                        features.Add(this.Create(ele));
+                    }
+                    else if (node is XText && node.Parent.Name == "date")
+                    {
+                        features.AddRange(this.Create((XText)node));
+                    }
+                }
+
+                yield return features.ToArray();
+            }
+        }
+
+        private TeiFeature Create(XElement element)
+        {
+            return TeiFeature.Create(element.Value, element.Name.LocalName, 0);
+        }
+
+        private IEnumerable<TeiFeature> Create(XText text)
+        {
+            return text.Value.SplitWithDelims(PdfToXml.Constants.FullPunctuation)
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select((x, i) => TeiFeature.Create(x, "other", i));
+        }
+    }
+}
---
+++ test/Grobid.Test/Tei/DateFeatureFactoryTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+
+using Grobid.NET;
+
+namespace Grobid.Test.Tei
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class DateFeatureFactoryTest
+    {
+        [Fact]
+        public void DateFeatureFactoryTest00()
+        {
+            var tei = @"<dates><date><month>March</month> <day>28</day>, <year>2007</year></date></dates>";
+
+            var testSubject = new DateFeatureFactory();
+            var formatter = new TeiFeatureFormatter();
+
+            Approvals.Verify(formatter.CreateString(testSubject.Create(XDocument.Parse(tei)).First()));
+        }
+
+        [Fact]
+        public void DateFeatureFactoryTest01()
+        {
+            var tei = @"<dates>
+  <date><month>March</month> <day>28</day>, <year>2007</year></date>
+  <date>Published <day>25</day> <month>May</month> <year>2011</year></date>
+</dates>";
+
+            var testSubject = new DateFeatureFactory();
+            var formatter = new TeiFeatureFormatter();
+
+            Approvals.VerifyAll(
+                testSubject.Create(XDocument.Parse(tei)),
+                x => formatter.CreateString(x) + "\n");
+        }
+    }
+}
